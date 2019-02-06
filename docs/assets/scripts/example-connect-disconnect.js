bc4eec88c350351c3f78c54af7525ff046df3be7
Yining Shi <ys2189@nyu.edu>
2019-02-05 23:58:08 -0500

    Deploy website
    Deploy website version based on 93e5ef622fcdecc94f3e0c8c7f0fdd440373f2e3

--- docs/assets/scripts/example-connect-disconnect.js
+++ docs/assets/scripts/example-connect-disconnect.js
@@ -5,23 +5,28 @@
 
 // The serviceUuid must match the serviceUuid of the device you would like to connect
 const serviceUuid = "19b10010-e8f2-537e-4f6c-d104768a1214";
-let myCharacteristic;
-let myValue = 0;
 let myBLE;
+let isConnected = false;
 
 function setup() {
   // Create a p5ble class
   myBLE = new p5ble();
 
-  const canvas = createCanvas(200, 200);
-  canvas.parent('canvasContainer');
+  canvas = createCanvas(200, 200);
+  canvas.parent('#canvasContainer');
+
   textSize(20);
   textAlign(CENTER, CENTER);
 
   // Create a 'Connect' button
   const connectButton = createButton('Connect')
   connectButton.mousePressed(connectToBle);
-  connectButton.parent('canvasContainer')
+  connectButton.parent('#canvasContainer');
+
+  // Create a 'Disconnect' button
+  const disconnectButton = createButton('Disconnect')
+  disconnectButton.mousePressed(disconnectToBle);
+  disconnectButton.parent('#canvasContainer');
 }
 
 function connectToBle() {
@@ -29,29 +34,36 @@ function connectToBle() {
   myBLE.connect(serviceUuid, gotCharacteristics);
 }
 
+function disconnectToBle() {
+  // Disonnect to the device
+  myBLE.disconnect();
+  // Check if myBLE is connected
+  isConnected = myBLE.isConnected();
+}
+
+function onDisconnected() {
+  console.log('Device got disconnected.');
+  isConnected = false;
+}
+
 // A function that will be called once got characteristics
 function gotCharacteristics(error, characteristics) {
   if (error) console.log('error: ', error);
   console.log('characteristics: ', characteristics);
-  myCharacteristic = characteristics[0];
-  // Read the value of the first characteristic
-  myBLE.read(myCharacteristic, gotValue);
-}
 
-// A function that will be called once got values
-function gotValue(error, value) {
-  if (error) console.log('error: ', error);
-  console.log('value: ', value);
-  myValue = value;
-  // After getting a value, call p5ble.read() again to get the value again
-  myBLE.read(myCharacteristic, gotValue);
-  // You can also pass in the dataType
-  // Options: 'unit8', 'uint16', 'uint32', 'int8', 'int16', 'int32', 'float32', 'float64', 'string'
-  // myBLE.read(myCharacteristic, 'string', gotValue);
+  // Check if myBLE is connected
+  isConnected = myBLE.isConnected();
+
+  // Add a event handler when the device is disconnected
+  myBLE.onDisconnected(onDisconnected)
 }
 
 function draw() {
-  background(250);
-  // Write value on the canvas
-  text(myValue, 100, 100);
+  if (isConnected) {
+    background(0, 255, 0);
+    text('Connected!', 100, 100);
+  } else {
+    background(255, 0, 0);
+    text('Disconnected :/', 100, 100);
+  }
 }
--- docs/assets/scripts/example-start-stop-notifications.js
+++ docs/assets/scripts/example-start-stop-notifications.js
@@ -13,18 +13,24 @@ function setup() {
   // Create a p5ble class
   myBLE = new p5ble();
 
-  const canvas = createCanvas(200, 200);
-  canvas.parent('canvasContainer');
+  canvas = createCanvas(200, 200);
+  canvas.parent('#canvasContainer');
+
   textSize(20);
   textAlign(CENTER, CENTER);
 
-  // Create a 'Connect' button
-  const connectButton = createButton('Connect')
-  connectButton.mousePressed(connectToBle);
-  connectButton.parent('canvasContainer')
+  // Create a 'Connect and Start Notifications' button
+  const connectButton = createButton('Connect and Start Notifications')
+  connectButton.mousePressed(connectAndStartNotify);
+  connectButton.parent('#canvasContainer');
+
+  // Create a 'Stop Notifications' button
+  const stopButton = createButton('Stop Notifications');
+  stopButton.mousePressed(stopNotifications);
+  stopButton.parent('#canvasContainer');
 }
 
-function connectToBle() {
+function connectAndStartNotify() {
   // Connect to a device by passing the service UUID
   myBLE.connect(serviceUuid, gotCharacteristics);
 }
@@ -34,20 +40,23 @@ function gotCharacteristics(error, characteristics) {
   if (error) console.log('error: ', error);
   console.log('characteristics: ', characteristics);
   myCharacteristic = characteristics[0];
-  // Read the value of the first characteristic
-  myBLE.read(myCharacteristic, gotValue);
-}
-
-// A function that will be called once got values
-function gotValue(error, value) {
-  if (error) console.log('error: ', error);
-  console.log('value: ', value);
-  myValue = value;
-  // After getting a value, call p5ble.read() again to get the value again
-  myBLE.read(myCharacteristic, gotValue);
+  // Start notifications on the first characteristic by passing the characteristic
+  // And a callback function to handle notifications
+  myBLE.startNotifications(myCharacteristic, handleNotifications);
   // You can also pass in the dataType
   // Options: 'unit8', 'uint16', 'uint32', 'int8', 'int16', 'int32', 'float32', 'float64', 'string'
-  // myBLE.read(myCharacteristic, 'string', gotValue);
+  // myBLE.startNotifications(myCharacteristic, handleNotifications, 'string');
+}
+
+// A function that will be called once got characteristics
+function handleNotifications(data) {
+  console.log('data: ', data);
+  myValue = data;
+}
+
+// A function to stop notifications
+function stopNotifications() {
+  myBLE.stopNotifications(myCharacteristic);
 }
 
 function draw() {
